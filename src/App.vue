28341b72e2e4856df153498eec57c4e9e9e0b21d
Jorge A. Lopez <Jorgelopez5252@gmail.com>
2023-01-15 18:03:54 -0700

    vue app is now running

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <div id="app">
     <!-- inner piece of app -->
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search.."/>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      api_key: 'a511288c93ca905f4ba9b1a0eda9231d'
+    }
+  }
 }
 </script>
 
