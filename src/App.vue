3292ca4942e574f246a08c98e9506b2404e48887
Jorge A. Lopez <Jorgelopez5252@gmail.com>
2023-01-16 22:46:59 -0700

    next step is to add markup

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <!-- inner piece of app -->
     <main>
       <div class="search-box">
-        <input type="text" class="search-bar" placeholder="Search.."/>
+        <input type="text" class="search-bar" placeholder="Search.." />
       </div>
     </main>
   </div>
@@ -13,7 +13,7 @@
 
 export default {
   name: 'App',
-  data () {
+  data() {
     return {
       api_key: 'a511288c93ca905f4ba9b1a0eda9231d'
     }
@@ -34,5 +34,44 @@ body {
 
 #app {
   background-image: url(./assets/cold-bg.jpg);
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+}
+
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  color: #313131;
+  font-size: 20px;
+
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255,255,255,0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255,255,255,0.75);
+  border-radius: 16px 0px 16px 0px;
 }
 </style>
