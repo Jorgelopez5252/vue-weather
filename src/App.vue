dd9c4a6f94a0d949746d25eb9b943e432580060e
Jorge A. Lopez <Jorgelopez5252@gmail.com>
2023-01-16 23:34:15 -0700

    almost done

--- src/App.vue
+++ src/App.vue
@@ -13,11 +13,11 @@
       <div class="weather-wrap" v-if="(typeof weather.main != 'undefined')">
         <div class="location-box">
           <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
-          <div class="date">Monday 16 January 2020</div>
+          <div class="date">{{ dateBuilder() }}</div>
         </div>
         <div class="weather-box">
-          <div class="temp">9°c</div>
-          <div class="weather">Rain</div>
+          <div class="temp">{{ Math.round(weather.main.temp)}}°f</div>
+          <div class="weather">{{  weather.weather[0].main }}</div>
         </div>
       </div>
     </main>
@@ -49,6 +49,18 @@ export default {
     },
     setResults(results) {
       this.weather = results;
+    },
+    dateBuilder () {
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`;
     }
   }
 }
@@ -70,6 +82,10 @@ body {
   background-size: cover;
   background-position: bottom;
   transition: 0.4s;
+}
+
+#app.warm{
+  background-image: url('./assets/warm-bg.jpg');
 }
 
 main {
